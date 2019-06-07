f6649d094ff2e2426a8a052a4b4e5c2630d4471e
zackzagone <51433870+zackzagone@users.noreply.github.com>
2019-06-06 17:23:28 -0700

    Update Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -3,7 +3,9 @@
 #prior to running this code
 #The environment variable TWITTER must also be set to the path containing the credentials text file'
 #the documentation for nltk and twitter can be found at http://www.nltk.org/howto/twitter.html
-from nltk.twitter import Twitter, credsfromfile
+from nltk.twitter import Twitter, credsfromfile, 
+from pprint import pprint
 oauth = credsfromfile()
 tw = Twitter()
-tw.tweets(keywords='game of thrones', to_screen=False, stream=False, limit=100)
+tw.tweets(keywords='shooting',  limit=10) #prints to terminal 
+tw.tweets(keywords='shooting',to_screen=False,  limit=10) #prints to file
